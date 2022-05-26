c61888eb35577665daa5be95331315dffb9355f4
workofartiz <arghhhhhthur@gmail.com>
2022-05-26 17:41:06 +0200

    simple python client concept added

---
+++ client/simple_python_client.py
@@ -0,0 +1,32 @@
+from argparse import ArgumentParser
+
+import socket
+import time
+import struct
+import hashlib
+
+def enable_ssh_port(ip, port, secret):
+    # Create UDP sock
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    timestamp = time.time()
+    m = hashlib.sha256()
+
+    # sha256(timestamp || secret)
+    m.update(struct.pack("<Q", timestamp))
+    m.update(SECRET)
+
+    sock.sendto(struct.pack("<Qp", timestamp, m.digest()))
+
+def main():
+    parser = ArgumentParser(description="simple unlocking client")
+    parser.add_argument('server', help="the ip of the remote server")
+    parser.add_argument('port', type=int, help="the port of the remote server's gatekeeper program")
+    parser.add_argument('secret', help="the secret used, to unlock the gatekeeper")
+
+    args = parser.parse_args()
+
+    enable_ssh_port(args.server, args.port, args.secret)
+
+if __name__ == '__main__':
+    main()
